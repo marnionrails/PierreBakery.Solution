5a3c07c02e400be456323b28f312b0893b733127
marnionrails <37088724+marnionrails@users.noreply.github.com>
2022-02-14 20:13:14 -0500

    pass test to get pastry quantity

--- PierreBakery.Tests/ModelTests/PastryTests.cs
+++ PierreBakery.Tests/ModelTests/PastryTests.cs
@@ -9,8 +9,16 @@ namespace PierreBakery.TestTools
     [TestMethod]
     public void PastryConstructor_CreateInstanceOfPastry_Pastry()
     {
-      Pastry testPastry = new Pastry();
+      Pastry testPastry = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), testPastry.GetType());
+    }
+    [TestMethod]
+    public void GetPastryQuantity_GetPastryQuantity_Int()
+    {
+      int pastryAmount = 1;
+      Pastry testPastry = new Pastry(pastryAmount);
+      int result = testPastry.Quantity;
+      Assert.AreEqual(pastryAmount, result);
     }
   }
 }
--- PierreBakery/Models/Pastry.cs
+++ PierreBakery/Models/Pastry.cs
@@ -2,6 +2,11 @@ namespace PierreBakery.Models
 {
   public class Pastry
   {
-    
+    public int Quantity { get; set; }
+  
+    public Pastry(int quantity)
+    {
+      Quantity = quantity;
+    }
   }
 }
